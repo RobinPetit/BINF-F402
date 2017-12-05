3a2a6357c02a64f940d98d17344ec047ca758f1b
RobinPetit <robpetit@ulb.ac.be>
2017-12-06 00:43:34 +0100

    [Assignment1] Question 4

--- assignment1.R
+++ assignment1.R
@@ -56,6 +56,7 @@ extract.chromosome <- function(annotations, chr) {
 	return (annotations[annotations$CHR == chr,]);
 }
 
+# Make the barplots of the average methylation level of each chromosome
 plot.mean.methylation.level <- function(infinium, annotations) {
 	means <- matrix(rep(0, 2*length(ALL_CHROMOSOMES)), nrow=2, ncol=length(ALL_CHROMOSOMES));
 	idx <- 1;
@@ -72,9 +73,22 @@ plot.mean.methylation.level <- function(infinium, annotations) {
 	barplot(means[,CHROMOSOMES_TO_TEST], names.arg=CHROMOSOMES_TO_TEST, beside=T, legend=c('Cases', 'Controls'), las=1);
 }
 
+# ...
+plot.number.of.genes.per.probe <- function(annotations) {
+	non.null.genes.associations <- as.vector(annotations[annotations$UCSC_RefGene_Name != '',]$UCSC_RefGene_Name);
+	genes.counter <- table(unlist(sapply(non.null.genes.associations, function(s) {unique(strsplit(s, ';'))})));
+	hist(genes.counter, main='Distribution of the number of probes per gene',
+		sub=sprintf('Average number of probes per gene is %g', mean(genes.counter)),
+		xlab='Number of probes per gene',
+		ylab='Frequency');
+	plot(log(table(genes.counter)), main='log plot of the distribution of the number of probes per gene',
+		xlab='Number of probes per gene',
+		ylab='log(Frequency)');
+}
+
 # Use first column as row names
 infinium <- read.table(INFINIUM_PATH, header=T, dec=',', row.names=1);
 annotations <- read.table(ANNO_MINI_PATH, header=T, sep=',', row.names=1);
 plot.beta.distribution.infinium(infinium);
 plot.mean.methylation.level(infinium, annotations);
-
+plot.number.of.genes.per.probe(annotations);
